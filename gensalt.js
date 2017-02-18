dc9c5202f98f471675fa50fa0642ac6db0d1ef1f
Ângelo Ocanã <angeloocana@gmail.com>
2017-02-18 10:03:22 -0200

    generating .env file on pretest

--- gensalt.js
+++ gensalt.js
@@ -2,13 +2,36 @@ var bcrypt = require('bcrypt');
 var fs = require('fs');
 
 var saltRounds = 10;
+var envFile = './.env';
 
+function envFileDoNotExists(next){
+    fs.readFile(envFile, 'utf8', function(err, data){
+        if(err)
+        {
+            console.log(err);
+            throw err;
+        }
+
+        if(!data)
+            next();
+
+        if(data.indexOf('PASSWORD_SALT') >= 0)
+            return;
+        else
+            next();
+    });
+}
+
+function generateSalt(){
 bcrypt.genSalt(saltRounds, function(err, salt){
     console.log('salt:', salt);
-    fs.writeFile('./.env', 'PASSWORD_SALT=' + salt, function(err){
+    fs.writeFile(envFile, 'PASSWORD_SALT=' + salt, function(err){
         if(err)
             return console.log('error writing salt to .env file:', err);
 
         console.log('salt saved to .env file');
     });
 });
+}
+
+envFileDoNotExists(generateSalt);
